bd0c1c1e8b067acda80ada0a1ca524a276313ad5
L-Dogg <szymek94sul@gmail.com>
2017-05-10 14:17:15 +0200

    Add helpers

---
+++ get_one_line.py
@@ -0,0 +1,9 @@
+import requests
+import json
+from Helpers import VehicleType
+
+
+APIKEY = 'b2404019-9b1d-4ff5-9969-af137b28c7da'
+ENDPOINT_URL = 'https://api.um.warszawa.pl/api/action/busestrams_get/?resource_id=f2e5503e-927d-4ad3-9500-4ab9e55deb59&' \
+               'apikey={}&type={}&line={}'
+
