3df9dd786f0b50ca3a349d49bcb7d2cd0b576d2d
L-Dogg <szymek94sul@gmail.com>
2017-05-25 10:47:37 +0200

    Initial tram report

--- heatmap.R
+++ heatmap.R
@@ -4,12 +4,12 @@ library(jsonlite)
 library(dplyr)
 library(lubridate)
 
-map <- get_map("Warszawa, Polska", zoom=11)
+map <- get_map("Warszawa, Polska", zoom=12)
 
-filename = 'data\\14-05\\trams3-filtered.json'
+filename = 'data/14-05/trams3-filtered.json'
 
 trams <- fromJSON(filename) %>%
-  filter(hour(Time) %in% c(8, 9, 10)) %>%
+  filter(hour(Time) %in% 6:23) %>%
   distinct() %>%
   arrange(Time)
 
@@ -42,13 +42,13 @@ normalPlot(normalBuses15)
 
 
 # Heat map
-heatmap <- function (df) { 
+heatmap <- function (df, title) { 
   ggmap(map, extent = "device") + geom_density2d(data = df, 
   aes(x = Lon, y = Lat), size = 0.3) + stat_density2d(data = df, 
   aes(x = Lon, y = Lat, fill = ..level.., alpha = ..level..), size = 0.01, 
   bins = 16, geom = "polygon") + scale_fill_gradient(low = "green", high = "red") + 
   scale_alpha(range = c(0, 0.3), guide = FALSE) +
-  ggtitle("Autobusy normalne 14.05.2017")
+  ggtitle(title)
 }
 
 heatmap(nightBuses14)
