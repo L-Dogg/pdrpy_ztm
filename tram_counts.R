317d93070310b87dbe1f0061d32ad401394886c0
L-Dogg <szymek94sul@gmail.com>
2017-05-27 15:07:50 +0200

    Add more info to the report

--- tram_counts.R
+++ tram_counts.R
@@ -29,3 +29,25 @@ directory = 'data/23-05/'
 df <- matrix(c(lowfloors, counts), ncol = 2)
 colnames(df) <- c('Lowfloor', 'All')
 write.csv(df, paste(directory, "tram-counts.csv", sep=""), na='', row.names = FALSE)
+
+##############################
+
+filename = 'data/15-05/trams3-filtered.json'
+trams <- fromJSON(filename) %>%
+  filter(hour(Time) %in% 0:23) %>%
+  distinct() %>%
+  arrange(hour(Time))
+
+counts <- rep(0, 24)
+for (i in 0:23)
+{
+  counts[i+1] <- nrow(trams %>%
+    filter(hour(Time) == i) %>%
+    group_by(Lines, Brigade) %>%
+    summarise(count = n()))
+}
+
+directory = 'data/15-05/'
+df <- matrix(c(0:23, counts), ncol = 2)
+colnames(df) <- c('Hour', 'Count')
+write.csv(df, paste(directory, "tram-counts.csv", sep=""), na='', row.names = FALSE)
